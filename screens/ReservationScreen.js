4adf16e3030eca727ee9fc891d72906a08a5d367
Josh Peterson <joshuadanpeterson@gmail.com>
2023-12-14 03:56:33 -0700

    Local Notifications

--- screens/ReservationScreen.js
+++ screens/ReservationScreen.js
@@ -11,6 +11,7 @@ import {
 import { Picker } from "@react-native-picker/picker";
 import DateTimePicker from "@react-native-community/datetimepicker";
 import * as Animatable from "react-native-animatable";
+import * as Notifications from "expo-notifications";
 
 const ReservationScreen = () => {
 	const [campers, setCampers] = useState(1);
@@ -38,7 +39,12 @@ const ReservationScreen = () => {
 				},
 				{
 					text: "OK",
-					onPress: () => resetForm(),
+					onPress: () => {
+						presentLocalNotification(
+							date.toLocaleDateString("en-US")
+						);
+						resetForm();
+					},
 				},
 			],
 			{ cancelable: false }
@@ -108,6 +114,33 @@ const ReservationScreen = () => {
 			</Animatable.View>
 		</ScrollView>
 	);
+};
+
+const presentLocalNotification = async (reservationDate) => {
+	const sendNotification = () => {
+		Notifications.setNotificationHandler({
+			handleNotification: async () => ({
+				shouldShowAlert: true,
+			}),
+		});
+
+		Notifications.scheduleNotificationAsync({
+			content: {
+				title: "Your Campsite Reservation Search",
+				body: `Search for ${reservationDate} requested`,
+			},
+			trigger: null,
+		});
+	};
+
+	let permissions = await Notifications.getPermissionsAsync();
+	if (!permissions.granted) {
+		permissions = await Notifications.requestPermissionsAsync();
+	}
+
+	if (permissions.granted) {
+		sendNotification();
+	}
 };
 
 const styles = StyleSheet.create({
